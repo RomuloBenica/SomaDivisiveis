d6c311f89480e23a9d0c3040ee7f65d1fbdd160c
RomuloBenica <romulobenica@hotmail.com>
2021-02-24 11:03:03 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function SumDivisible(numbers){
+    let cont = 0;
+
+    for(let i = 0; i < numbers; i++){
+        let array = JSON.stringify(i).split('');
+        let lastNumber = JSON.parse(array[array.length - 1]);
+
+        if(lastNumber === 0 || lastNumber === 5){
+            cont += i;
+        }
+    }
+
+    for(let i = 0; i < numbers; i++){
+        //verificar se e divisivel por 3
+        const result = (parseInt(i/3)*3) - i;
+
+        if(result === 0){
+            cont += i;
+        }
+    }
+    console.log(cont);
+    
+}
+
+
+SumDivisible(13);
